ff0acb6ddb06a76155b778dbe57689c56d92a609
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-04-09 02:58:51 -0500

    modified templates

--- gamified_improvement/gamified_improvement/settings.py
+++ gamified_improvement/gamified_improvement/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication settings
+LOGIN_REDIRECT_URL = 'core:index'
+LOGIN_URL = 'auth:login'
+LOGOUT_REDIRECT_URL = 'core:index'

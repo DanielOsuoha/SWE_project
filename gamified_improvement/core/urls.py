5c0009a269ff7d492d08d7f2eed3e8daadaee215
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-04-09 05:53:12 -0500

    modified templates

--- gamified_improvement/core/urls.py
+++ gamified_improvement/core/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('friends/', views.friends, name='friends'),
     path('profile/', views.profile, name='profile'),
     path('about/', views.about, name='about'),
+    path('index/', views.index, name='index'),
     path('profile/change-password/', views.change_password, name='change_password'),
     path('plan/<int:pk>/', views.plan_detail, name='plan_detail'),
     ]

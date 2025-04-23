a63aa3ba572c0996ff6a00836efabac3eb8e1b97
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-04-23 12:43:49 -0400

    modified files

--- gamified_improvement/core/models.py
+++ gamified_improvement/core/models.py
@@ -95,3 +95,13 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    if hasattr(instance, 'profile'):
+        instance.profile.save()

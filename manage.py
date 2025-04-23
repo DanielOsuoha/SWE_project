e3e5c8d6f3dd4ba36649b3e822b954c1d5b79fa8
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-04-23 11:23:32 -0400

    slight changes

--- manage.py
+++ manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gamified_improvement.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

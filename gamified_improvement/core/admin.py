34a66f5b08fcf6cc7c18b4cab847a0e6cd782fe0
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-04-23 09:42:55 -0400

    slight changes

--- gamified_improvement/core/admin.py
+++ gamified_improvement/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Plan
+from .models import Plan, Goal
 
+admin.site.register(Goal)
 @admin.register(Plan)
 class PlanAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'difficulty', 'rating')
